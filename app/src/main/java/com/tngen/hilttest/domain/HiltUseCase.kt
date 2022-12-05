3b42dc394a7bf1aa2c991319d2c7e520b2fb09f6
최마로 <>
2022-12-05 12:23:14 +0900

    PUSH

--- app/src/main/java/com/tngen/hilttest/domain/HiltUseCase.kt
+++ app/src/main/java/com/tngen/hilttest/domain/HiltUseCase.kt
@@ -4,7 +4,7 @@ import com.tngen.hilttest.data.entity.NoteEntity
 import com.tngen.hilttest.domain.repository.NoteRepository
 import javax.inject.Inject
 
-class HiltUseCase @Inject constructor(
+class HiltUseCase @Inject constructor( //내부 DB에 저장하기
     private val repository : NoteRepository
 ) : UseCase {
 
--- app/src/main/java/com/tngen/hilttest/domain/RetrofitUseCase.kt
+++ app/src/main/java/com/tngen/hilttest/domain/RetrofitUseCase.kt
@@ -8,7 +8,7 @@ import javax.inject.Inject
 class RetrofitUseCase
 @Inject constructor(
     private val houseRepository: HouseRepository
-){
+){ //Retrofit을 사용하여 접속하기, 데이터 가져오기
     suspend fun execute(){
         Log.d("RetrofitUseCase","${houseRepository.getHouse().items.count()}" )
     }
